8c26ca2df535b664df60305f840bc0c1319af1ad
Stefan Röhrbein <roehrbein@dial.de>
2016-09-09 11:21:34 +0200

    dependency update

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Version {
   final val Scala     = "2.11.8"
   final val ScalaTest = "3.0.0"
-  final val Akka      = "2.4.9"
+  final val Akka      = "2.4.10"
 }
 
 object Library {
